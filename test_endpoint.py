178e4eca313462714c842803be3ef876a54d6356
and3 windows <and3.sci@gmail.com>
2022-10-07 22:14:24 -0500

    i completed to write the app

---
+++ test_endpoint.py
@@ -0,0 +1,14 @@
+import requests
+url = "https://deployment-intro-nl-service-tkjyn7djeg-uc.a.run.app/v1/prediction"
+json = {
+    "opening_gross": 27520040.0, 
+    "screens": 3401.0, 
+    "production_budget": 130000000, 
+    "title_year": 2013.0, 
+    "aspect_ratio": 2.35, 
+    "duration": 100.0, 
+    "cast_total_facebook_likes": 14168, 
+    "budget": 130000000.0, 
+    "imdb_score": 4.9}
+
+breakpoint() # el breakpoint pausa el interprete 
